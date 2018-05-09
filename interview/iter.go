0c6ab8609f066fdfb2c403e7893636d15914cce0
maxwell92 <710028463@qq.com>
2018-05-09 23:16:47 +0800

    add answers

---
+++ interview/iter.go
@@ -0,0 +1,19 @@
+package main
+import(
+	"fmt"
+)
+func (set *threadSafeSet) Iter() <-chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		set.RLock()
+
+		for elem := range set.s {
+			ch <- elem
+		}
+
+		close(ch)
+		set.RUnlock()
+
+	}()
+	return ch
+}
