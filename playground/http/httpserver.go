93d26d57e2032b6363b0fe0571fcd6763b4f35d4
your namejvniki” <guolijv@163.com>
2018-05-09 20:55:06 +0800

    httpserver 简单实现

--- playground/http/httpserver.go
+++ playground/http/httpserver.go
@@ -1,1 +1,16 @@
-package http
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,
+		"Hi, This is an example of http service in golang!")
+}
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
--- playground/readfile/mystruct.go
+++ playground/readfile/mystruct.go
@@ -1,1 +1,16 @@
 package main
+
+type MyStruct struct {
+	N int
+}
+var n MyStruct
+//n := MyStruct{ 1 }
+// get
+immutable := reflect.ValueOf(n)
+val := immutable.FieldByName("N").Int()
+fmt.Printf("N=%d\n", val) // prints 1
+
+// set
+mutable := reflect.ValueOf(&n).Elem()
+mutable.FieldByName("N").SetInt(7)
+fmt.Printf("N=%d\n", n.N) // prints 7
